c9eab8f9f4db80718897e732c8a467fe72571784
Warren <warren.xia@robobloq.com>
2019-04-18 11:16:07 +0800

    feat: 1.2.6

--- www/build/static/gsrs/js/app.js
+++ www/build/static/gsrs/js/app.js
@@ -9,7 +9,7 @@ var LOCAL ={};
 var SY ={};
 SY.Client = 'pc';
 SEP.os ={};
-SEP.os.version = "1.2.5" ; //version
+SEP.os.version = "1.2.6" ; //version
 SEP.os.type = 1 ; // electron:1 ; web: 2; PC调用 GO接口： 3；（自动修正值，不用改）
 
 SEP.appInit = function () {
