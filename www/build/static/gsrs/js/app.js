e959ab53726ef37dc409857b620aafdb8019c853
Warren <warren.xia@robobloq.com>
2019-09-07 09:41:18 +0800

    feat: K1和K2绿旗和开始程序功能

--- www/build/static/gsrs/js/app.js
+++ www/build/static/gsrs/js/app.js
@@ -9,7 +9,7 @@ var LOCAL ={};
 var SY ={};
 SY.Client = 'pc';
 SEP.os ={};
-SEP.os.version = "1.3.3" ; //version
+SEP.os.version = "1.3.2" ; //version
 SEP.os.type = 1 ; // electron:1 ; web: 2; PC调用 GO接口： 3；（自动修正值，不用改）
 
 SEP.appInit = function () {
